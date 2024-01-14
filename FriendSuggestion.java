dcadecb320da9000e8256d328594ba81cd6fd7f9
trantuanthinh <tran.tuan.thinh.0125@gmail.com>
2024-01-14 20:16:10 +0700

    commit

--- FriendSuggestion.java
+++ FriendSuggestion.java
@@ -2,84 +2,68 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.InputMismatchException;
 import java.util.List;
 
-//60
-public class FacebookFriend {
+//100
+public class FriendSuggestion {
 
     static InputReader reader;
 
     public static void main(String[] args) throws IOException {
         reader = new InputReader(System.in);
         StringBuilder stringBuilder = new StringBuilder();
+
         int verticesNumber = reader.nextInt();
         int edgesNumber = reader.nextInt();
+        int limit = reader.nextInt();
 
         Vertex[] verticesList = readGraph(verticesNumber, edgesNumber);
-
-        for (int i = 1; i < verticesList.length; i++) {
-            if (verticesList[i].gender.equalsIgnoreCase("Nam")) {
-                stringBuilder.append(verticesList[i].getNumberOfFemale() + " ");
-            } else {
-                stringBuilder.append(verticesList[i].getNumberOfMale() + " ");
+        for (Vertex vertex : verticesList) {
+            Collections.sort(vertex.adjacentVertices,
+                    (vertex1, vertex2) -> Integer.compare(((Vertex) vertex1).id, ((Vertex) vertex2).id));
+            stringBuilder.append(vertex.id).append(" ");
+            for (Vertex vertex_2 : vertex.adjacentVertices) {
+                if (vertex_2.getDegree() < limit) {
+                    stringBuilder.append(vertex_2.id).append(" ");
+                }
             }
+            stringBuilder.append("\n");
         }
         System.out.print(stringBuilder);
     }
 
     static public Vertex[] readGraph(int numberOfVertices, int numberOfEdges) throws IOException {
-        Vertex[] verticesList = new Vertex[numberOfVertices + 1];
-        for (int i = 1; i < verticesList.length; i++) {
+        Vertex[] verticesList = new Vertex[numberOfVertices];
+        for (int i = 0; i < verticesList.length; i++) {
             int id = i;
-            String gender = reader.nextLine();
-            verticesList[id] = new Vertex(id, gender);
+            verticesList[id] = new Vertex(id);
         }
 
         for (int i = 0; i < numberOfEdges; i++) {
             int u = reader.nextInt();
             int v = reader.nextInt();
-
-            if (verticesList[u].gender.equalsIgnoreCase("Nam")) {
-                verticesList[v].addMaleAdjacentVertex(verticesList[u]);
-            } else {
-                verticesList[v].addFemaleAdjacentVertex(verticesList[u]);
-            }
-
-            if (verticesList[v].gender.equalsIgnoreCase("Nam")) {
-                verticesList[u].addMaleAdjacentVertex(verticesList[v]);
-            } else {
-                verticesList[u].addFemaleAdjacentVertex(verticesList[v]);
-            }
+            verticesList[v].addAdjacentVertex(verticesList[u]);
+            verticesList[u].addAdjacentVertex(verticesList[v]);
         }
         return verticesList;
     }
 
     static public class Vertex {
         private int id;
-        private String gender;
-        private List<Vertex> maleFriendList = new ArrayList<>();
-        private List<Vertex> femaleFriendList = new ArrayList<>();
+        private List<Vertex> adjacentVertices = new ArrayList<>();
 
-        public Vertex(int id, String gender) {
+        public Vertex(int id) {
             this.id = id;
-            this.gender = gender;
         }
 
-        public int getNumberOfMale() {
-            return this.maleFriendList.size();
+        public int getDegree() {
+            return this.adjacentVertices.size();
         }
 
-        public int getNumberOfFemale() {
-            return this.femaleFriendList.size();
-        }
-
-        public void addMaleAdjacentVertex(Vertex vertex) {
-            this.maleFriendList.add(vertex);
-        }
-
-        public void addFemaleAdjacentVertex(Vertex vertex) {
-            this.femaleFriendList.add(vertex);
+        public void addAdjacentVertex(Vertex vertex) {
+            this.adjacentVertices.add(vertex);
         }
 
         public String toString() {
